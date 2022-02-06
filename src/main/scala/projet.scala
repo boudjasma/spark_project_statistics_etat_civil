0b76fed03868d3a6e0b2c99522df5ccfadac5e8c
boudjasma <asma.boudjatat@gmail.com>
2022-02-06 03:32:12 +0100

    export tha data in csv files

--- src/main/scala/projet.scala
+++ src/main/scala/projet.scala
@@ -49,25 +49,25 @@ object project extends App {
                             .withColumnRenamed("count", "Population")
                             .withColumnRenamed("Country", "Country4")
 
-  val statistics_part_one = Married_by_country
+  val statistics_by_countries_part_one = Married_by_country
                               .join(Divorced_by_country, Married_by_country("Country1") === Divorced_by_country("Country2"), "cross")
 
-  val statistics_part_two = statistics_part_one
-                            .join(Single_by_country, statistics_part_one("Country1") === Single_by_country("Country3"), "cross")
+  val statistics_by_countries_part_two = statistics_by_countries_part_one
+                            .join(Single_by_country, statistics_by_countries_part_one("Country1") === Single_by_country("Country3"), "cross")
 
-  val statistics_part_three = statistics_part_two
-                            .join(population_by_country, statistics_part_two("Country1") === population_by_country("Country4"), "cross")
+  val statistics_by_countries_part_three = statistics_by_countries_part_two
+                            .join(population_by_country, statistics_by_countries_part_two("Country1") === population_by_country("Country4"), "cross")
 
   //All Statistics
-  val statistics = statistics_part_three
+  val statistics_by_countries = statistics_by_countries_part_three
                               .select(col("Country1").as("Country")
                                 , col("Population")
                                 , col("Married")
                                 , col("Divorced")
                                 , col("Single"))
 
-  statistics.printSchema()
-  statistics.show(10)
+  statistics_by_countries.printSchema()
+  statistics_by_countries.show(10)
 
   // Ten countries wich have a maximum number of married
    val ten_countries_max_married = Married_by_country
@@ -92,12 +92,12 @@ object project extends App {
 //
 //
 ////   statistics.repartition(1).write.csv("C://Users//asmab//OneDrive//Bureau//ESGI//spark//proje//statistics//statistics.csv")
-statistics.repartition(1)
-          .write
-          .format("csv")
-          .mode(SaveMode.Overwrite)
-          .option("header", "true")
-          .save("statistics/statistics.csv")
+statistics_by_countries.repartition(1)
+    .write
+    .format("csv")
+    .mode(SaveMode.Overwrite)
+    .option("header", "true")
+  .save("C://Users//asmab//OneDrive//Bureau//ESGI//spark//projet//statistics//statistics.csv")
 //
   //Analyse with age
   val Married_by_age = All
@@ -105,17 +105,20 @@ statistics.repartition(1)
                       .groupBy("AgeGroup")
                       .count()
                       .withColumnRenamed("count","NumberOfMarried")
+                      .withColumnRenamed("AgeGroup","AgeGroup1")
 
   val Divorced_by_age = All
                       .filter(All("MaritalStatus") like "Divorced")
                       .groupBy("AgeGroup").count()
                       .withColumnRenamed("count","NumberOfDivorced")
+                      .withColumnRenamed("AgeGroup","AgeGroup2")
 
   val Single_by_age = All
                       .filter(All("MaritalStatus") like "Single")
                       .groupBy("AgeGroup")
                       .count()
                       .withColumnRenamed("count","NumberOfSingle")
+                      .withColumnRenamed("AgeGroup","AgeGroup3")
 
   val max_married_by_age = Married_by_age
                       .sort(desc("NumberOfMarried"))
@@ -131,6 +134,40 @@ statistics.repartition(1)
                     .sort(desc("NumberOfSingle"))
                     .limit(5)
   max_Single_by_age.show()
+
+  val population_by_age = All
+    .groupBy("AgeGroup")
+    .count()
+    .withColumnRenamed("count", "Population")
+    .withColumnRenamed("AgeGroup", "AgeGroup4")
+
+
+  val statistics_by_age_part_one = Married_by_age
+    .join(Divorced_by_age, Married_by_age("AgeGroup1") === Divorced_by_age("AgeGroup2"), "cross")
+
+  val statistics_by_age_part_two = statistics_by_age_part_one
+    .join(Single_by_age, statistics_by_age_part_one("AgeGroup1") === Single_by_age("AgeGroup3"), "cross")
+
+  val statistics_by_age_part_three = statistics_by_age_part_two
+    .join(population_by_age, statistics_by_age_part_two("AgeGroup1") === population_by_age("AgeGroup4"), "cross")
+
+  //All Statistics
+  val statistics_by_age = statistics_by_age_part_three
+    .select(col("AgeGroup1").as("AgeGroup")
+      , col("Population")
+      , col("NumberOfMarried")
+      , col("NumberOfDivorced")
+      , col("NumberOfSingle")
+    )
+
+  statistics_by_age.show(10)
+
+  statistics_by_age.repartition(1)
+    .write
+    .format("csv")
+    .mode(SaveMode.Overwrite)
+    .option("header", "true")
+    .save("C://Users//asmab//OneDrive//Bureau//ESGI//spark//projet//statistics//statistics_by_age.csv")
 
   System.in.read
   ss.stop()
